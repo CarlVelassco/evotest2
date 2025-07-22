b79724a7f8fbaa429cd3c34c0ac29214c7c97f41
CarlVelassco <hochyevhen@gmail.com>
2025-07-22 04:46:07 +0200

    34634е6

--- server/index.js
+++ server/index.js
@@ -1,33 +1,15 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 
+const express = require('express');
+const path = require('path');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
 
-io.on("connection", (socket) => {
-  console.log("Пользователь подключен:", socket.id);
+app.use(express.static(path.join(__dirname, 'dist')));
 
-  socket.on("createLobby", () => {
-    console.log("Лобби создано");
-  });
-
-  socket.on("joinLobby", ({ lobbyId }) => {
-    console.log("Присоединение к лобби:", lobbyId);
-  });
-});
-
-app.use(express.static("dist")); // если билд Vite
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
